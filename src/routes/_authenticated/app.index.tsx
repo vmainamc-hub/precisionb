54b0db43302f7d5f8f55b84c0ce49d8b261bfb2a
vmainamc-hub <vmainamc@gmail.com>
2026-09-18 16:27:27 +0300

    Make DTrader the default app workspace

--- src/routes/_authenticated/app.index.tsx
+++ src/routes/_authenticated/app.index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/app/")({
   beforeLoad: () => {
-    throw redirect({ to: "/app/apex" });
+    throw redirect({ to: "/app/dtrader" });
   },
 });
